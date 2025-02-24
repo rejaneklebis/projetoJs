b72ef2eb02b144491060e3b9328df0e7d1b97415
rejaneklebis <128099393+rejaneklebis@users.noreply.github.com>
2025-02-23 22:56:12 -0300

    Semana1

---
+++ semana1/exer4_tipo_dado/script.js
@@ -0,0 +1,9 @@
+let dado = prompt("Digite um dado qualquer:");
+
+if (confirm("Deseja verificar o tipo do dado informado?")) {
+   
+    alert(typeof(dado));
+} else {
+    
+    alert("<h2>Obrigado por visitar esta página</h2>");
+}
