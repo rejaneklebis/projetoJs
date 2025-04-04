fbb05cd0b209a88062a23bd1442ac5b1b1ea4f1e
rejaneklebis <128099393+rejaneklebis@users.noreply.github.com>
2025-04-04 14:10:02 -0300

    Delete semana1 directory

--- semana1/exer4_tipo_dado/script.js
+++
@@ -1,9 +0,0 @@
-let dado = prompt("Digite um dado qualquer:");
-
-if (confirm("Deseja verificar o tipo do dado informado?")) {
-   
-    alert(typeof(dado));
-} else {
-    
-    alert("<h2>Obrigado por visitar esta página</h2>");
-}
